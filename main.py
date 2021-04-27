6851fb45a35cf90c3617f0a213c9ab3190a4db03
Donoras <donoras@gmail.com>
2021-04-28 00:02:22 +0300

    Deleted trash

--- main.py
+++ main.py
@@ -5,37 +5,34 @@ import datetime
 
 ROOT = f'http://{HOST}:{PORT}/xmlrpc/'
 
-uid = xmlrpc.client.ServerProxy(ROOT + 'common').login(DB,USER,PASS)
+uid = xmlrpc.client.ServerProxy(ROOT + 'common').login(DB, USER, PASS)
 print(f"Logged in as {USER} (uid:{uid})")
 
 call = functools.partial(
     xmlrpc.client.ServerProxy(ROOT + 'object').execute,
     DB, uid, PASS)
 
+
 def print_all_projects():
     # Read the projects
-    projects = call('dn_projects.project', 'search_read', [], ['name', 'description', 'start_date', 'end_date', 'client_id', 'leader_id'])
+    projects = call('dn_projects.project', 'search_read', [],
+                    ['name', 'description', 'start_date', 'end_date', 'client_id', 'leader_id'])
 
     for project in projects:
-        print(f"Projektas {project['id']} {project['name']}, nuo: {project['start_date']} iki {project['end_date']} klientas: {project['client_id'][1] if project['client_id'] else None}, vadovas: {project['leader_id'][1] if project['leader_id'] else None}")
+        print(
+            f"Projektas {project['id']} {project['name']}, nuo: {project['start_date']} iki {project['end_date']} klientas: {project['client_id'][1] if project['client_id'] else None}, vadovas: {project['leader_id'][1] if project['leader_id'] else None}")
+
 
 def print_all_clients():
     clients = call('res.partner', 'search_read', [], ['name'])
     for client in clients:
         print(client['id'], client['name'])
 
+
 def print_all_leaders():
-    leaders = call('hr.employee', 'search_read', [('leader','=',True)], ['name'])
+    leaders = call('hr.employee', 'search_read', [('leader', '=', True)], ['name'])
     for leader in leaders:
         print(leader['id'], leader['name'])
-
-# def create_project():
-
-# 3.create a new session
-
-# course_id = call('openacademy.course', 'search', [('name','ilike','Course 3')])[0]
-# instructor_id = call('res.partner', 'search', [('name','ilike','Admin')])[0]
-#
 
 
 while True:
@@ -64,7 +61,7 @@ while True:
                 'end_date': end_date,
                 'client_id': client_id,
                 'leader_id': leader_id,
-        })
+            })
             print(f"Projektas {name} įrašytas")
         except:
             print("Nepavyko įrašyti projekto")
